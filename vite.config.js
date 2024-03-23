ff818142863d11ba861f3250f5e7bf965db9a6ea
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-03-23 12:49:38 -0500

    Last changes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true
+      }
     }
   }
+
+  if (command !== 'serve') {
+    config.base = 'state-machines'
+  }
+
+  return config;
 })
